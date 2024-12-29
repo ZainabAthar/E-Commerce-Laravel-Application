df47d612cc371e96a7149c3e93232136ace7c576
Zainab Athar <118754216+ZainabAthar@users.noreply.github.com>
2024-12-29 12:50:09 +0500

    Working Code

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,50 @@
+<div class="divider"></div>
+<footer class="main px-10">
+    <section class="section-padding footer-mid">
+        <div class="container pt-15 pb-20">
+            <div class="row flex justify-between">
+                <div class="col-lg-4 col-md-6">
+                    <div class="widget-about font-md mb-md-5 mb-lg-0">
+                        <div class="logo logo-width-1 wow fadeIn animated">
+                            <a href="index.html"><img src="{{ asset('assets/imgs/logo/logo.png')}}" alt="logo"></a>
+                        </div>
+                        <h5 class="mt-20 mb-10 fw-600 text-grey-4 wow fadeIn animated">Contact</h5>
+                        <p class="wow fadeIn animated">
+                            <strong>Address: </strong>Rural Area X
+                        </p>
+                        <p class="wow fadeIn animated">
+                            <strong>Phone: </strong>+212 682860421
+                        </p>
+                        <p class="wow fadeIn animated">
+                            <strong>Email: </strong>rural-web@gmail.com
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</footer>
+<script src="{{ asset('assets/js/vendor/modernizr-3.6.0.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/jquery-3.6.0.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/jquery-migrate-3.3.0.min.js') }}"></script>
+<script src="{{ asset('assets/js/vendor/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/slick.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/jquery.syotimer.min.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/wow.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/jquery-ui.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/perfect-scrollbar.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/magnific-popup.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/select2.min.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/waypoints.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/counterup.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/jquery.countdown.min.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/images-loaded.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/isotope.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/scrollup.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/jquery.vticker-min.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/jquery.theia.sticky.js') }}"></script>
+<script src="{{ asset('assets/js/plugins/jquery.elevatezoom.js') }}"></script>
+<!-- Template JS -->
+<script src="{{ asset('assets/js/main.js?v=3.3') }}"></script>
+<script src="{{ asset('assets/js/shop.js?v=3.3') }}"></script>
+@stack('scripts')
